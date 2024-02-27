750acb89410d5ee60668c6cc5ab8065d83a6aa90
Francisco Oliveira <fransoliveira@gmail.com>
2024-02-27 13:40:43 -0300

    Adicionar arquivos de projeto.

---
+++ MvcWebApplication/Data/MvcWebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcWebApplication.Models;
+
+namespace MvcWebApplication.Data
+{
+    public class MvcWebApplicationContext : DbContext
+    {
+        public MvcWebApplicationContext (DbContextOptions<MvcWebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcWebApplication.Models.Aluno> Aluno { get; set; } = default!;
+    }
+}
