960106f3f02b067c1d70917aa1f97294f39a1bbe
Francisco Oliveira <fransoliveira@gmail.com>
2024-02-27 13:59:50 -0300

    feat: Nova Francis

---
+++ MvcWebApplication/Models/Secr.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcWebApplication.Models
+{
+    public class Secr
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public DateTime DataNasc { get; set; }
+    }
+}
